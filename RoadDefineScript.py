842c18aa4ec82d34f6d3dc404cc6e70f19adab65
Maciej <mechlinskimaciej@gmail.com>
2024-01-10 17:31:06 +0100

    poprawki po Kacprze

--- RoadDefineScript.py
+++ RoadDefineScript.py
@@ -666,7 +666,7 @@ def Collecting_Road_Translated_Map(Map,neighbours, new_road, roads, state):
                         occurences += sub.count(neighbour)
                 if (neighValue <40 and (new_road[-1] in new_neighbours or new_road[0] in new_neighbours) and not any(neighbour in sublist for sublist in roads)) or (((neighValue < 100 and neighValue >= 40) or (neighValue >=150 and neighValue <160)) and (new_road[-1] in new_neighbours or new_road[0] in new_neighbours) and occurences <2) or (((neighValue >= 100 and neighValue <150) or neighValue >=160) and (new_road[-1] in new_neighbours or new_road[0] in new_neighbours)):
                     if neighValue >=160:
-                        return
+                        continue
                     else:
                         if(new_road[-1] in new_neighbours):
                             new_neighbours.remove(new_road[-1])
@@ -728,6 +728,6 @@ def Additional_Translate(Map,neighbour,new_road,roads,new_neighbours, neighValue
             new_neighbours.remove(new_road[0])
             new_road.insert(0, neighbour)
         Collecting_Road_Translated_Map(Map, new_neighbours, new_road, roads, state)
-Map = Template_Map()
-new_roads = Main_Algorithm_Translated_Map(Map)
-print(new_roads)
+#Map = Template_Map()
+#new_roads = Main_Algorithm_Translated_Map(Map)
+#print(new_roads)
